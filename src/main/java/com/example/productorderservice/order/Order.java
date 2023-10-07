c52940495de3fdd6bea117534e7e7a15487a71dd
tasklet1579 <tasklet1579@gmail.com>
2023-10-07 21:21:30 +0900

    JPA 적용하기

--- src/main/java/com/example/productorderservice/order/Order.java
+++ src/main/java/com/example/productorderservice/order/Order.java
@@ -1,12 +1,24 @@
 package com.example.productorderservice.order;
 
 import com.example.productorderservice.product.Product;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.util.Assert;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "orders")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private final Product product;
-    private final int quantity;
+    @OneToOne
+    private Product product;
+    private int quantity;
 
     public Order(Product product, int quantity) {
         Assert.notNull(product, "상품은 필수입니다.");
@@ -14,13 +26,5 @@ class Order {
         this.product = product;
         this.quantity = quantity;
 
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void assignId(Long id) {
-        this.id = id;
     }
 }
--- src/main/java/com/example/productorderservice/order/OrderRepository.java
+++ src/main/java/com/example/productorderservice/order/OrderRepository.java
@@ -1,17 +1,9 @@
 package com.example.productorderservice.order;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @Repository
-class OrderRepository {
-    private final Map<Long, Order> persistence = new HashMap<>();
-    private Long sequence = 0L;
+interface OrderRepository extends JpaRepository<Order, Long> {
 
-    public void save(Order order) {
-        order.assignId(++sequence);
-        persistence.put(order.getId(), order);
-    }
 }
