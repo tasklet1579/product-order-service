e9c5fc41bb14738de03fb4d942f1196ed3710e0e
tasklet1579 <tasklet1579@gmail.com>
2023-10-07 20:44:04 +0900

    POJO 상품 주문 기능 구현하기

--- src/main/java/com/example/productorderservice/product/ProductRepository.java
+++ src/main/java/com/example/productorderservice/product/ProductRepository.java
@@ -1,11 +1,7 @@
 package com.example.productorderservice.product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-import java.util.HashMap;
-import java.util.Map;
-
-interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
